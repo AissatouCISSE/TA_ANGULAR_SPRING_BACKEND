33fb61fb5c4d40e1b8b334868388d83fa4d51a5f
AissatouCISSE <aissatoucisse351@gmail.com>
2020-11-22 21:15:31 +0100

    Transfert_Argent_Partie_Spring

--- src/main/java/sn/simplon/transfert_argent/controller/ClientController.java
+++ src/main/java/sn/simplon/transfert_argent/controller/ClientController.java
@@ -3,6 +3,7 @@ package sn.simplon.transfert_argent.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ import sn.simplon.transfert_argent.repository.EmetteurRepository;
 import sn.simplon.transfert_argent.repository.EnvoieRepository;
 import sn.simplon.transfert_argent.repository.RecepteurRepository;
 
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/api/transfert")
 public class ClientController {
@@ -38,22 +39,26 @@ public class ClientController {
 			envoieRepository.save(envoie);
 }
 		@PostMapping("/envoyer")
-		public void encapsulatrion( String nom_emetteur, String prenom_emetteur, int  tel_emetteur, int cin, 
-				String nom_recepteur, String prenom_recepteur, int tel_recepteur, String date, int montant) {
+		public void encapsulation( String nom_emetteur, String prenom_emetteur, Integer  tel_emetteur, Integer cin, 
+				String nom_recepteur, String prenom_recepteur, Integer tel_recepteur, String date, Integer montant) {
 			Emetteur emetteur=new Emetteur(); 
 			emetteur.setNom_emetteur(nom_emetteur);
 			emetteur.setPrenom_emetteur(prenom_emetteur);
-			emetteur.setTel_emetteur(tel_emetteur);
-			emetteur.setCin(cin);
+			emetteur.setTel_emetteur(tel_emetteur.intValue());
+			emetteur.setCin(cin.intValue());
+			
+			
 			
 			Recepteur recepteur=new Recepteur();
 			recepteur.setNom_recepteur(nom_recepteur);
 			recepteur.setPrenom_recepteur(prenom_recepteur);
-			recepteur.setTel_recepteur(tel_recepteur);
+			recepteur.setTel_recepteur(tel_recepteur.intValue());
 			
 			Envoie envoie=new Envoie();
 			envoie.setDate(date);
-			envoie.setMontant(montant);
+			envoie.setMontant(montant.intValue());
 			envoyer(emetteur, recepteur, envoie);
-		}
+			
+			//emetteur.setCin(envoie.getEmetteur().getCin());
+			}
 }
